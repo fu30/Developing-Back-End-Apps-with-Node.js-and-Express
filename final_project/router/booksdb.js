0e71277896557176bd21eaa350f5d404ed7ebf79
fu30 <156079299+fu30@users.noreply.github.com>
2024-01-10 12:32:58 +0800

    Create booksdb.js

---
+++ final_project/router/booksdb.js
@@ -0,0 +1,23 @@
+let books = {
+    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": [] },
+    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": [
+        {
+            "username": "can",
+            "review":"This is a review comment"
+        },
+        {
+            "username": "Jane Smith",
+            "review":"This is a review comment from 2nd user"
+        }
+    ]},
+    3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": [] },
+    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": [] },
+    5: {"author": "Unknown","title": "The Book Of Job", "reviews": [] },
+    6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": [] },
+    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": [] },
+    8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": [] },
+    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": [] },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [] }
+}
+
+module.exports=books;
